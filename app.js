d85d7ee1cb0177e57ce3465d6681ba3b48811455
Chris Pryor <chrispryor@live.com>
2018-11-30 00:03:31 -0700

    add files to site

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+});
